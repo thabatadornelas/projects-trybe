1e85fb8e28a88730e56c75445a84534b7e3c6e0f
Thabata Dornelas <62298224+thabatadornelas@users.noreply.github.com>
2021-01-28 15:15:35 -0300

    Create index.js

---
+++ sd-04-stranger-things-backend/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE === 'false');
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(req.query, hereIsTheUpsideDown);
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta: ${PORT}`);
+});
